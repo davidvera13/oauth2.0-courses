16137d154f6c693d7d3f62a8aa84d70f4122f59d
davidvera13 <david.vera@9online.fr>
2025-05-10 16:41:30 +0200

    feat(resource): scope based access control impl

--- oauth-2.0-in-spring-boot-applications/step 02 - scope based access control/resource-server/src/main/java/eu/dreamlabs/resources/controllers/UserController.java
+++ oauth-2.0-in-spring-boot-applications/step 02 - scope based access control/resource-server/src/main/java/eu/dreamlabs/resources/controllers/UserController.java
@@ -2,15 +2,29 @@ package eu.dreamlabs.resources.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final Environment env;
+
+    @GetMapping
+    public String getUserWithScopeProfile(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> claims = jwt.getClaims();
+        System.out.println("----------------------------------------");
+        claims.forEach((claimName, claimValue) -> {
+            System.out.println(claimName + ": " + claimValue);
+        });
+        return "Working on port " + env.getProperty("server.port");
+    }
 
     @GetMapping("/status")
     public String status() {

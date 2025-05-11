049f22570a45b9ba43ea090851f4ea59320a359c
davidvera13 <david.vera@9online.fr>
2025-05-11 20:22:39 +0200

    feat(resource): oauth2.0 in MVC web app impl wuth webclient

--- oauth-2.0-in-spring-boot-applications/step 06 - mvc web app/web-app/src/main/java/eu/dreamlabs/webapp/controllers/AppController.java
+++ oauth-2.0-in-spring-boot-applications/step 06 - mvc web app/web-app/src/main/java/eu/dreamlabs/webapp/controllers/AppController.java
@@ -8,14 +8,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -23,47 +26,65 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AppController {
     private final OAuth2AuthorizedClientService oauth2ClientService;
-    private final RestTemplate restTemplate;
+    // private final RestTemplate restTemplate;
+    private final WebClient webClient;
+
 
     @GetMapping("/albums")
     public String getAlbums(
-            Model model) {
-            // @AuthenticationPrincipal OidcUser principal) {
-        //
-        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-        OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(),
-                oauthToken.getName());
-        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
-        System.out.println("jwtAccessToken = " + jwtAccessToken);
-        //System.out.println("Principal = " + principal);
-        //OidcIdToken idToken = principal.getIdToken();
-        //String idTokenValue = idToken.getTokenValue();
-        //System.out.println("idTokenValue = " + idTokenValue);
+            Model model,
+            @AuthenticationPrincipal OidcUser principal) {
         String url = "http://localhost:8765/api/albums";
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + jwtAccessToken);
-        HttpEntity<List<AlbumResponse>> entity = new HttpEntity<>(headers);
-        ResponseEntity<List<AlbumResponse>> responseEntity =  restTemplate.exchange(
-                url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {});
-        List<AlbumResponse> albums = responseEntity.getBody();
-        // case 1: testing display data in html
-        //AlbumResponse album1 = AlbumResponse.builder()
-        //        .albumId("a1")
-        //        .userId("1")
-        //        .albumTitle("Album 1")
-        //        .albumDescription("Album 1 details")
-        //        .albumUrl("http://url1.com")
-        //        .build();
-        //AlbumResponse album2 = AlbumResponse.builder()
-        //        .albumId("a2")
-        //        .userId("2")
-        //        .albumTitle("Album 2")
-        //        .albumDescription("Album 2 details")
-        //        .albumUrl("http://url2.com")
-        //        .build();
-        //model.addAttribute("albums", Arrays.asList(album1, album2));
+
+        List<AlbumResponse> albums = webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<AlbumResponse>>(){})
+                .block();
+
         model.addAttribute("albums", albums);
         return "albums";
     }
+
+//    @GetMapping("/albums")
+//    public String getAlbums(
+//            Model model) {
+//            // @AuthenticationPrincipal OidcUser principal) {
+//        //
+//        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+//        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+//        OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(),
+//                oauthToken.getName());
+//        String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+//        System.out.println("jwtAccessToken = " + jwtAccessToken);
+//        //System.out.println("Principal = " + principal);
+//        //OidcIdToken idToken = principal.getIdToken();
+//        //String idTokenValue = idToken.getTokenValue();
+//        //System.out.println("idTokenValue = " + idTokenValue);
+//        String url = "http://localhost:8765/api/albums";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Authorization", "Bearer " + jwtAccessToken);
+//        HttpEntity<List<AlbumResponse>> entity = new HttpEntity<>(headers);
+//        ResponseEntity<List<AlbumResponse>> responseEntity =  restTemplate.exchange(
+//                url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {});
+//        List<AlbumResponse> albums = responseEntity.getBody();
+//        // case 1: testing display data in html
+//        //AlbumResponse album1 = AlbumResponse.builder()
+//        //        .albumId("a1")
+//        //        .userId("1")
+//        //        .albumTitle("Album 1")
+//        //        .albumDescription("Album 1 details")
+//        //        .albumUrl("http://url1.com")
+//        //        .build();
+//        //AlbumResponse album2 = AlbumResponse.builder()
+//        //        .albumId("a2")
+//        //        .userId("2")
+//        //        .albumTitle("Album 2")
+//        //        .albumDescription("Album 2 details")
+//        //        .albumUrl("http://url2.com")
+//        //        .build();
+//        //model.addAttribute("albums", Arrays.asList(album1, album2));
+//        model.addAttribute("albums", albums);
+//        return "albums";
+//    }
 }

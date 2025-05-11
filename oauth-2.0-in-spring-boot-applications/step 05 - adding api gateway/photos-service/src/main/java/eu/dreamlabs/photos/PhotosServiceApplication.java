39e4c4b1bc9ae720ffbceb9cb5bf7cd53e27b305
davidvera13 <david.vera@9online.fr>
2025-05-11 11:34:41 +0200

    feat(resource): adding basic resources servers

---
+++ oauth-2.0-in-spring-boot-applications/step 05 - adding api gateway/photos-service/src/main/java/eu/dreamlabs/photos/PhotosServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.dreamlabs.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotosServiceApplication.class, args);
+    }
+
+}

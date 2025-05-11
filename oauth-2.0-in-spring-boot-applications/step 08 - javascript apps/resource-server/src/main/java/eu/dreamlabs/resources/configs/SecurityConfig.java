91b1e3f5c8d05a99a697c0ea6e9c76476ae006f1
davidvera13 <david.vera@9online.fr>
2025-05-12 01:07:11 +0200

    feat(resource): Oauth 2.0 with Javascript pkce impl

--- oauth-2.0-in-spring-boot-applications/step 08 - javascript apps/resource-server/src/main/java/eu/dreamlabs/resources/configs/SecurityConfig.java
+++ oauth-2.0-in-spring-boot-applications/step 08 - javascript apps/resource-server/src/main/java/eu/dreamlabs/resources/configs/SecurityConfig.java
@@ -3,11 +3,17 @@ package eu.dreamlabs.resources.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled=true, prePostEnabled=true)
@@ -22,6 +28,8 @@ public class SecurityConfig {
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         return http
+                // cors can't be configured in gateway and resource at the same time: generate error...
+                //.cors(Customizer.withDefaults())
                 // we require all calls to webservice to be authenticated
                 // for users endpoint, scope must have profile scope
                 .authorizeHttpRequests(auth -> auth
@@ -34,4 +42,15 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter)))
                 .build();
     }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Collections.singletonList("*"));
+//        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//        configuration.setAllowedMethods(Collections.singletonList("*"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
